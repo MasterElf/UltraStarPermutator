b08c9fe6bbe6fe72364f0943a988559eb9b48c72
Mats Elfving <mats.elfving@gmail.com>
2022-09-25 00:23:33 +0200

    Första enkla incheckning

---
+++ UltraStarPermutator/Model/AudioModel.cs
@@ -0,0 +1,23 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UltraStarPermutator
+{
+    public class AudioModel : ObservableObject
+    {
+        private string? filePath;
+
+        public string? FilePath
+        {
+            get => filePath;
+            set => SetProperty(ref filePath, value);
+        }
+
+        private string? name;
+
+        public string? Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+    }
+}
---
+++ UltraStarPermutator/Model/ProjectModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UltraStarPermutator
+{
+    public class ProjectModel : ObservableObject
+    {
+        private string? sourceUltraStarTextFilePath;
+
+        public string? SourceUltraStarTextFilePath
+        {
+            get => sourceUltraStarTextFilePath;
+            set => SetProperty(ref sourceUltraStarTextFilePath, value);
+        }
+    }
+}
